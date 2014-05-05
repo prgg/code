1aaa6e50703404cdaadc3d75449f58da0e7cc336
rong.peng <rong.peng@langtaojin.com>
2014-05-05 17:14:05 +0800

    add 20140505

---
+++ demo/20140505/data.js
@@ -0,0 +1,5 @@
+﻿var img = new Image();
+img.onload = function(){
+	document.body.innerHTML = "http://www.j.cn/js/1x1.gif?ucs=UTF-8&un=statistic_channel.test.mobile.0";
+};
+img.src = "http://www.j.cn/js/1x1.gif?ucs=UTF-8&un=statistic_channel.test.mobile.0";
